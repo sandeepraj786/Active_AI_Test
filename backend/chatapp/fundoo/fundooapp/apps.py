2ef3b04ff8a5162b7e7903531239200da47679ea
SANDEEP RAJ <sandeeprajsingh05@gmail.com>
2021-06-06 21:26:17 +0530

    Delete backend directory

--- backend/chatapp/fundoo/fundooapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FundooappConfig(AppConfig):
-    name = 'fundooapp'
--- backend/chatapp/scripts/django.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-sudo systemctl daemon-reload 
-sudo systemctl start django
-sudo systemctl enable django 
